8724dbe01b2d8c35bdc8128bc741c0e1c6a46c8e
Sikundi <sikundi499@gmail.com>
2023-02-17 17:24:39 +0300

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digits and follow with new line
+ *
+ * Return: Always 0 (succcess)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+
+	putchar("\n");
+
+	return (0);
+}
