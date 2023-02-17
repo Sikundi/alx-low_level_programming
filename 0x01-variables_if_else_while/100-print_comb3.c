4bb862ebe8a0b22449ac20ed39084e80bbb28da9
Sikundi <sikundi499@gmail.com>
2023-02-17 22:47:56 +0300

    combinations of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,18 +16,16 @@ int main(void)
 		putchar((num1 % 10) + '0');
 		putchar((num2 % 10) + '0');
 
-		if (num1 != 8 || (num1 == 8 && num2 != 9)
-			continue;
+		if (num1 != 8 || (num1 == 8 && num2 != 9))
+		{
 
 		putchar(',');
 		putchar(' ');
 		}
+	}
 	}
 
 	putchar('\n');
 
 	return (0);
 }
-
-
-
