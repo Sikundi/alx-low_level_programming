5e284a41cca6ccf1f352b4250b259badae86cfe1
Sikundi <sikundi499@gmail.com>
2023-02-21 22:47:23 +0300

    addition

---
+++ 0x02-functions_nested_loops/10_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - plus two intergers
+ * @num1: first interger
+ * @num2: second interger
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2
+
+	return (sum);
+}
