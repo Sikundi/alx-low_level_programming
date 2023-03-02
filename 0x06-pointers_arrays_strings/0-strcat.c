50d1456b8e289b1d09a64e39a6ca9c9571093cbb
Sikundi <sikundi499@gmail.com>
2023-03-02 16:59:19 +0300

    function that links two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * strcat - concatenate(arrange/link) two strings
+ * @dest: first character
+ * @src: second character
+ * Return: A pointer resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *g = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (g);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+**This folder deals with more about pointers, arrays and strings in the C Programming language.**
