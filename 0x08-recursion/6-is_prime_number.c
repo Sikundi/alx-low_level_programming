b4d0e1bb8512a4cf8e78b4ab21ac81c36e5d3925
Sikundi <sikundi499@gmail.com>
2023-03-08 23:42:48 +0300

    return 1 if interger is a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,13 +10,13 @@
 */
 int is_divisible(int num, int div)
 {
-if (num % div == 0)
-return (0);
+	if (num % div == 0)
+	return (0);
 
-if (div == num / 2)
-return (1);
+	if (div == num / 2)
+	return (1);
 
-return (is_divisible(num, div + 1));
+	return (is_divisible(num, div + 1));
 }
 
 /**
@@ -28,13 +28,13 @@ return (is_divisible(num, div + 1));
 */
 int is_prime_number(int n)
 {
-int div = 2;
+	int div = 2;
 
-if (n <= 1)
-return (0);
+	if (n <= 1)
+	return (0);
 
-if (n >= 2 && n <= 3)
-return (1);
+	if (n >= 2 && n <= 3)
+	return (1);
 
-return (is_divisible(n, div));
+	return (is_divisible(n, div));
 }
