037d0e62492ad3cbfa5e93152b733c8dd5a72dba
Sikundi <sikundi499@gmail.com>
2023-03-12 18:08:03 +0300

    Program that prints it's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: the argument count
+ * @argv: an array of pointers to the arguments
+ * Return: Always 0 (success)
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+**This is a folder on the arguments argc and argv**
