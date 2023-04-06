967a544d93d19908a7e39af78b659f8f284619eb
Sikundi <sikundi499@gmail.com>
2023-04-06 23:36:58 +0300

    Function converts binary num to unsigned interger

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,14 +18,14 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			return(0);
+			return (0);
 	}
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		num <<=1;
+		num <<= 1;
 		if (b[i] == '1')
-			num +=1;
+			num += 1;
 	}
 	return (num);
 }
